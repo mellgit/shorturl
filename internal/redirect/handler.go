b589856f744df59b74d00ba63204d2bdb5cbc232
mellgit <daniilhorn8@gmail.com>
2025-04-28 22:33:07 +0300

    refactor: delete code comments

--- internal/redirect/handler.go
+++ internal/redirect/handler.go
@@ -16,9 +16,9 @@ func NewHandler(service *Service, logger *log.Entry) *Handler {
 }
 
 func (h *Handler) GroupHandler(app *fiber.App) {
+
 	group := app.Group("/api", middleware.JWTProtected())
 	group.Get("/s/:alias", h.RedirectHandler)
-
 }
 
 func (h *Handler) RedirectHandler(ctx *fiber.Ctx) error {
@@ -36,19 +36,3 @@ func (h *Handler) RedirectHandler(ctx *fiber.Ctx) error {
 	//return ctx.Redirect(original, fiber.StatusFound)
 	return ctx.JSON(fiber.Map{"original": original})
 }
-
-//
-//func RedirectHandler(s *Service) fiber.Handler {
-//	return func(c *fiber.Ctx) error {
-//		alias := c.Params("alias")
-//		ip := c.IP()
-//		ua := c.Get("User-Agent")
-//
-//		original, err := s.ResolveAndTrack(alias, ip, ua)
-//		if err != nil {
-//			return fiber.NewError(fiber.StatusNotFound, err.Error())
-//		}
-//
-//		return c.Redirect(original, fiber.StatusFound)
-//	}
-//}
