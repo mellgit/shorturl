e285c96aa2573be49345da371a7f21b2b515b3ad
mellgit <daniilhorn8@gmail.com>
2025-05-05 13:08:26 +0300

    chore: change id format to uuid for auth, urls and clicks in models

--- internal/auth/model.go
+++ internal/auth/model.go
@@ -1,9 +1,11 @@
 package auth
 
+import "github.com/google/uuid"
+
 type User struct {
-	ID       int64  `json:"id"`
-	Email    string `json:"email"`
-	Password string `json:"-"` // Храним в зашифрованном виде
+	ID       uuid.UUID `json:"id"`
+	Email    string    `json:"email"`
+	Password string    `json:"-"` // Храним в зашифрованном виде
 }
 
 type RegisterRequest struct {
