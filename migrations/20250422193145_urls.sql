802c9ee9e2ca0b4fffdb6b885ed79521d7664b18
mellgit <daniilhorn8@gmail.com>
2025-05-05 13:07:39 +0300

    chore: change id format to uuid for auth, urls and clicks in migration

--- migrations/20250422193145_urls.sql
+++ migrations/20250422193145_urls.sql
@@ -1,12 +1,13 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE urls (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL,
+    id UUID DEFAULT gen_random_uuid() NOT NULL,
+    user_id UUID NOT NULL,
     original TEXT NOT NULL,
     alias TEXT UNIQUE NOT NULL,
     expires_at TIMESTAMP NOT NULL,
-    created_at TIMESTAMP NOT NULL
+    created_at TIMESTAMP NOT NULL,
+    primary key(id)
 );
 
 -- +goose StatementEnd
