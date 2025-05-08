94bc30b12773f472e665a5cc2fe64344f9b52854
mellgit <daniilhorn8@gmail.com>
2025-05-08 15:45:47 +0300

    chore: add comments for login method

--- internal/auth/service.go
+++ internal/auth/service.go
@@ -27,6 +27,7 @@ func (s *AuthService) Register(email, password string) error {
 		return fmt.Errorf("email already registered")
 	}
 
+	// get hash from password for save in db
 	hashed, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
 		return fmt.Errorf("could not hash password: %w", err)
@@ -45,20 +46,23 @@ func (s *AuthService) Login(email, password string) (string, error) {
 		return "", fmt.Errorf("could not find user by email: %w", err)
 	}
 
+	// compare the password hash in the database and from the user
 	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
 	if err != nil {
 		return "", fmt.Errorf("could not compare password: %w", err)
 	}
 
+	// data token
 	claims := jwt.MapClaims{
 		"user_id": user.ID,
 		"exp":     time.Now().Add(time.Hour * 72).Unix(),
 	}
 
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	secret := os.Getenv("JWT_SECRET")
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims) // create new token (algorithm signing HMAC-SHA256)
+	secret := os.Getenv("JWT_SECRET")                          // secret token
 
-	signedToken, err := token.SignedString([]byte(secret))
+	// use secret key for sing token
+	signedToken, err := token.SignedString([]byte(secret)) // header.payload.signature
 	if err != nil {
 		return "", fmt.Errorf("could not sign token: %w", err)
 	}
